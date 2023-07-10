4d2a956190238ecddfe8d00a5c85e97a33a7cfa7
Amy Smith <smithniamyfse@gmail.com>
2023-07-10 09:17:49 -0500

    update header title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header-bg-container">
       <header className="App-header">
-        <h1 className="App-title">It's raining, right?</h1>
+        <h1 className="App-title">That's rain, right?</h1>
       </header>
       <div className="description-container">
         <p><span className="no-text">No.</span> It may be a different kind of downpour if you are under a tree and feel raindrops.</p>
